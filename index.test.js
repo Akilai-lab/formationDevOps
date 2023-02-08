bd501136f286802c601bf707f7765a7e1ba39e05
unknown <akilaroha@hotmail.fr>
2023-02-08 14:11:15 +0100

    tests not validates

--- index.test.js
+++ index.test.js
@@ -5,13 +5,13 @@ describe('maths module', () => {
     expect(add(1, 2)).toBe(3);
   });
   test('subtract 1 - 2 to equal 1', () => {
-    expect(subtract(1, 2)).toBe(-1);
+    expect(subtract(1, 2)).toBe(7);
   });
   test('multiply 1 * 2 to equal 2', () => {
     expect(multiply(1, 2)).toBe(2);
   });
-  test('divide 10 / 2 to equal 5', () => {
-    expect(divide(10, 2)).toBe(5);
+  test('divide 10 / 2 to equal 10', () => {
+    expect(divide(10, 2)).toBe(10);
   });
   test('divide 10 / 0 cannot divide by zero', () => {
     expect(()=>divide(10, 0).toThrow('Cannot divide by zero'));
