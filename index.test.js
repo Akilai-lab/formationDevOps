925ffe4c55fd6f38a5311e2e58cd734857a3a4db
unknown <akilaroha@hotmail.fr>
2023-02-08 14:09:48 +0100

    test

---
+++ index.test.js
@@ -0,0 +1,19 @@
+import {add, subtract, multiply, divide} from './index';
+
+describe('maths module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+  test('subtract 1 - 2 to equal 1', () => {
+    expect(subtract(1, 2)).toBe(-1);
+  });
+  test('multiply 1 * 2 to equal 2', () => {
+    expect(multiply(1, 2)).toBe(2);
+  });
+  test('divide 10 / 2 to equal 5', () => {
+    expect(divide(10, 2)).toBe(5);
+  });
+  test('divide 10 / 0 cannot divide by zero', () => {
+    expect(()=>divide(10, 0).toThrow('Cannot divide by zero'));
+  });
+});
