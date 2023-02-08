a38e94c4d24f23e52805061455c9e325705a0a10
unknown <akilaroha@hotmail.fr>
2023-02-08 15:05:36 +0100

    change manip

--- index.test.js
+++ index.test.js
@@ -11,7 +11,7 @@ describe('maths module', () => {
     expect(multiply(1, 2)).toBe(2);
   });
   test('divide 10 / 2 to equal 5', () => {
-    expect(divide(10, 2)).toBe(20);
+    expect(divide(10, 2)).toBe(5);
   });
   test('divide 10 / 0 cannot divide by zero', () => {
     expect(()=>divide(10, 0).toThrow('Cannot divide by zero'));
