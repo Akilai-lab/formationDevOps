6caf7f2b41451c2b24f6e24091d7fb2a7e60302c
unknown <akilaroha@hotmail.fr>
2023-02-08 14:24:36 +0100

    test error

--- index.test.js
+++ index.test.js
@@ -11,7 +11,7 @@ describe('maths module', () => {
     expect(multiply(1, 2)).toBe(2);
   });
   test('divide 10 / 2 to equal 10', () => {
-    expect(divide(10, 2)).toBe(10);
+    expect(divide(10, 2)).toBe(20);
   });
   test('divide 10 / 0 cannot divide by zero', () => {
     expect(()=>divide(10, 0).toThrow('Cannot divide by zero'));
